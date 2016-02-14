820973b5106ec0e755da7efdd646f3ec48953480
szwchao <szwchao@gmail.com>
2016-02-14 14:54:27 +0800

    第一版

---
+++ firmware.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, re
+import socket
+import threading
+import tools.tftpy as tftpy
+from tools.progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar, RotatingMarker
+import logging
+import getopt
+import argparse
+from fw import Firmware_Downloader
+from tools.colorama import init
+from tools.colorama import Fore, Back, Style
+
+init(autoreset=True)
+
+def comment(str):
+    print(Fore.GREEN + str)
+
+class MyError(Exception):
+    pass
+
+def run(server_ip, client_ip):
+    f = Firmware_Downloader(url=client_ip, server_ip=server_ip)
+    comment('Old version is: ' + f.get_version())
+
+    widgets = ['Transferring: ', Percentage(), ' ', Bar(marker=RotatingMarker()), ' ', ETA(), ' ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=3670000).start()
+
+    server = tftpy.TftpServer('.', hook=pbar.update)
+    server_thread = threading.Thread(target=server.listen, kwargs={'listenip': server_ip, 'listenport': 69, 'timeout': 10})
+    server_thread.start()
+    try:
+        server.is_running.wait()
+        f.download()
+        f.wait()
+        pbar.finish()
+        f.reboot()
+        f.confirm()
+        comment('New version is: ' + f.get_version())
+    finally:
+        server.stop(now=False)
+        server_thread.join()
+
+def checkip(ip):
+    if not re.match('^(([01]?\d\d?|2[0-4]\d|25[0-5])\.){3}([01]?\d\d?|2[0-4]\d|25[0-5])$', ip):
+        raise MyError, 'Not a valid ip address!'
+
+def guess_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
+    local_ip = s.getsockname()[0]
+    return local_ip
+
+def main(args):
+    if args.server:
+        server_ip = args.server
+    else:
+        server_ip = guess_local_ip()
+    client_ip = args.client
+    checkip(server_ip)
+    checkip(client_ip)
+
+    comment("server ip: %s" % (server_ip))
+    comment("controller ip: %s" % (client_ip))
+    if not client_ip.startswith('http://'):
+        client_ip = 'http://' + client_ip
+
+    if args.loglevel == 0:
+        tftpy.setLogLevel(logging.NOTSET)
+    elif args.loglevel == 1:
+        tftpy.setLogLevel(logging.INFO)
+    elif args.loglevel == 2:
+        tftpy.setLogLevel(logging.DEBUG)
+    run(server_ip, client_ip)
+
+if __name__ == '__main__':
+    des = 'a tool which can help to download firmware to DC'
+    parser = argparse.ArgumentParser(description = des)
+    parser.add_argument('-s', '--server', type=str, help='server ip')
+    parser.add_argument('-c', '--controller', type=str, help='controller ip', required=True)
+    parser.add_argument('-l', '--loglevel', type=int, default=0, help='log level, 0=no log, 1=info, 2=debug')
+ 
+    args = parser.parse_args();
+    main(args)
---
+++ fw.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import time
+import socket
+from tools.colorama import init
+from tools.colorama import Fore, Back, Style
+
+if sys.version_info < (2, 7):
+    raise RuntimeError('At least Python 2.7 is required')
+try:#python2
+    from urllib import urlencode as urlencode
+    import urllib2 as request
+except ImportError:#python3
+    from urllib.parse import urlencode as urlencode
+    import urllib.request as request
+
+DEFAULT_URL = "192.168.0.2"
+
+def info(str):
+    print(Fore.RED + str)
+
+class FwError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class Firmware_Downloader(object):
+    username = 'admin'
+    password = 'admin'
+    cmd = {'form_id':'firmware_update', 'reboot_value':'', 'tftp_server_ip_address':'192.168.0.1', 'submit': 'Start', 'reboot': 'Reboot'}
+
+    def __init__(self, url=None, server_ip='192.168.0.1'):
+        self.url = url if url else self.guess_ip()
+        self.cmd['tftp_server_ip_address'] = server_ip
+
+    def auth(self, url):
+        p = request.HTTPPasswordMgrWithDefaultRealm()
+        p.add_password(None, url, self.username, self.password)
+        handler = request.HTTPBasicAuthHandler(p)
+        opener = request.build_opener(handler)
+        request.install_opener(opener)
+
+    def post(self, posturl, params):
+        '''
+            do the POST action of form in html
+
+        :param posturl: POST url
+        :param params: dict to orgnize the parameters that needed in post action
+        :return:
+        '''
+        # self.auth(posturl)
+        data = urlencode(params)
+        req = request.Request(posturl, data)
+        response = request.urlopen(req, timeout=20)
+
+    def get(self, url):
+        '''
+            do the GET action of form in html
+
+        :param url: GET url
+        :return:
+        '''
+        self.auth(url)
+        return request.urlopen(url, timeout=20).read()
+
+    def guess_ip(self):
+        begin_ip = "http://192.168.0."
+        found = False
+        for i in range(0, 256):
+            if found:
+                break
+            url = begin_ip + str(i) + "/firmware_update.html"
+            self.auth(url)
+            try:
+                result = request.urlopen(url, timeout=1).read()
+                found = True
+            except:
+                found = False
+        if found:
+            url = begin_ip + str(i-1)
+            info("found correct ip: %s" % url)
+        else:
+            raise FwError("can't find controller's ip")
+        return url
+
+    def get_version(self):
+        url = self.url + "/firmware_update.html"
+        result = self.get(url)
+        # '<div style="border-bottom:2px solid #D3D3D3; padding:0px 0px 5px 0px; margin-bottom:15px"><a href="index.html">Home</a> &middot; <a href="firmware_update.html">Firmware</a> &middot; <small>v03.17.00, compiled:&nbsp;Feb  4 2016/09:10:31<br/></small></div>'
+        compiled_index = result.index('compiled')
+        version = result[compiled_index-11:compiled_index-2]
+        # info(version)
+        return version
+        
+
+    def wait(self, timeout=300):
+        '''
+            wait the download procedure complete
+
+        :param timeout: timeout of download procedure
+        :return:
+        '''
+        start_time = time.time()
+        geturl = self.url + "/get.cgi?firmware_update=status"
+        current_time = time.time()
+        result = self.get(geturl)
+        while 'SUCCESS' not in result and (current_time-start_time) < timeout:
+            if 'ERROR' in result:
+                info("something error, can't download firmware!!!")
+                raise FwError(result)
+            time.sleep(5)
+            current_time = time.time()
+            result = self.get(geturl)
+            # info(result)
+
+        if (current_time-start_time) >= timeout:
+            raise FwError("Timeout!!!")
+
+        # info("Download complete, elapsed time: %ds" % (current_time-start_time))
+
+    def download(self):
+        # auth this url to connect firstly, otherwise post timeout
+        url = self.url + "/firmware_update.html"
+        self.auth(url)
+        posturl = self.url + "/post.cgi"
+        download_cmd = self.cmd
+        self.post(posturl, download_cmd)
+
+    def reboot(self):
+        ''' send reboot command to controller '''
+        posturl = self.url + "/post.cgi"
+        reboot_cmd = self.cmd
+        reboot_cmd['reboot_value'] = 'do_it'
+        try:
+		    self.post(posturl, reboot_cmd)
+        except:
+            # after reboot, controller will lost response
+            info("rebooting...")
+
+    def confirm(self):
+        ''' Poll request from url after reboot to confirm that firmware upgrade is complete.  '''
+        start_time = time.time()
+        eplapsed_time = start_time
+        while (eplapsed_time - start_time) < 120:
+            geturl = self.url + "/get.cgi?firmware_update=status"
+            try:
+                result = self.get(geturl)
+            except:
+                result = "error"
+            finally:
+                if "idle" in result.lower():
+                    info("download successful!!!")
+                    break
+            eplapsed_time = time.time()
+
+
+def firmware_download(server, client):
+    f = Firmware_Downloader(url=client, server_ip=server)
+    f.get_version()
+    # f.download()
+    # f.wait()
+    # f.reboot()
+    # f.confirm()
+
+
+if __name__ == '__main__':
+    # get local ip
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
+    local_ip = s.getsockname()[0]
+    firmware_download(local_ip, 'http://10.208.7.168')
