4fdf46dc44ebd13d9fa17df1a9832f7c676a2249
szwchao <szwchao@gmail.com>
2016-03-22 11:46:24 +0800

    更改目录结构

--- src/fw_upgrader.py
+++ src/fw_upgrader.py
@@ -4,9 +4,8 @@ import os, sys, re
 import socket
 import threading
 import logging
-import argparse
-from lib.progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar, RotatingMarker
-from firmware import Firmware_Downloader
+from .lib.progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar, RotatingMarker
+from .firmware import Firmware_Downloader
 
 py3 = sys.version_info[0] >= 3
 if py3:
@@ -60,31 +59,3 @@ def guess_local_ip():
     s.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
     local_ip = s.getsockname()[0]
     return local_ip
-
-def main(args):
-    if args.server:
-        server_ip = args.server
-    else:
-        server_ip = guess_local_ip()
-    client_ip = args.controller
-
-    if args.loglevel == 0:
-        loglevel = logging.NOTSET
-    elif args.loglevel == 1:
-        loglevel = logging.INFO
-    elif args.loglevel == 2:
-        loglevel = logging.DEBUG
-    fw_upgrader('.', server_ip, client_ip, args.timeout, args.web_response_timeout, args.noreboot, loglevel)
-
-if __name__ == '__main__':
-    des = 'a tool which can help to download firmware to DC'
-    parser = argparse.ArgumentParser(description = des)
-    parser.add_argument('-s', '--server', type=str, help='server ip')
-    parser.add_argument('-c', '--controller', type=str, help='controller ip', required=True)
-    parser.add_argument('-l', '--loglevel', type=int, default=0, help='log level, 0=no log, 1=info, 2=debug')
-    parser.add_argument('-t', '--timeout', type=int, default=10, help='timeout for tftp server')
-    parser.add_argument('-w', '--web_response_timeout', type=int, default=20, help='timeout for web response')
-    parser.add_argument('-r', '--noreboot', action='store_false', help='reboot controller if enable')
- 
-    args = parser.parse_args();
-    main(args)
